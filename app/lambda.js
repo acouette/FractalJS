8b664c9c3444f42a5564eb13422aabd2203cc6e6
Antoine Couette <a.j.couette@gmail.com>
2019-02-14 10:10:57 +0100

    update serverless ok

---
+++ app/lambda.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const awsServerlessExpress = require('aws-serverless-express');
+
+const app = require('./js/node/http/server'); // NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below, then redeploy (`npm run package-deploy`)
+
+
+const binaryMimeTypes = ['image/png'];
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
